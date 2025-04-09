864b245279d5fcc792d8f7414f40bda2e8ad4389
Zak <djabazaki@gmail.com>
2025-04-09 12:04:16 +0100

    Credits

--- ChessGUI.java
+++ ChessGUI.java
@@ -1,24 +1,25 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
-import javax.swing.*;
-import javax.swing.border.*;
 import java.net.URL;
 import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.*;
 
 public class ChessGUI {
 
     private final JPanel gui = new JPanel(new BorderLayout(3, 3));
+    JButton creditsButton = new JButton("Credits");
     private JButton[][] chessBoardSquares = new JButton[8][8];
     private Image[][] chessPieceImages = new Image[2][6];
     private JPanel chessBoard;
     private final JLabel message = new JLabel(
-            "Chess Champ is ready to play!");
+            "Good game!");
     private static final String COLS = "ABCDEFGH";
     public static final int QUEEN = 0, KING = 1,
             ROOK = 2, KNIGHT = 3, BISHOP = 4, PAWN = 5;
     public static final int[] STARTING_ROW = {
-        ROOK, KNIGHT, BISHOP, KING, QUEEN, BISHOP, KNIGHT, ROOK
+            ROOK, KNIGHT, BISHOP, KING, QUEEN, BISHOP, KNIGHT, ROOK
     };
     public static final int BLACK = 0, WHITE = 1;
 
@@ -49,17 +50,38 @@ public class ChessGUI {
         tools.add(new JButton("Resign")); // TODO - add functionality!
         tools.addSeparator();
         tools.add(message);
+        tools.add(creditsButton);
+        creditsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Créer une nouvelle fenêtre pour les crédits
+                JFrame creditsFrame = new JFrame("Credits");
+                creditsFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                creditsFrame.setSize(300, 220);
 
-        gui.add(new JLabel("?"), BorderLayout.LINE_START);
+                JPanel creditsPanel = new JPanel();
+                creditsPanel.setLayout(new BoxLayout(creditsPanel, BoxLayout.Y_AXIS));
+
+                creditsPanel.add(new JLabel("Made by:"));
+                creditsPanel.add(new JLabel("DJABA Zaki"));
+                creditsPanel.add(new JLabel("BOUAFIA Salah"));
+                creditsPanel.add(new JLabel("TALAMALI Mohamed Amir"));
+                creditsPanel.add(new JLabel("BENZETTA Sami"));
+                creditsPanel.add(new JLabel("OMEIRI Djasser"));
+                creditsPanel.add(new JLabel("OMEIRI Djasser"));
+                creditsPanel.add(new JLabel("OMEIRI Djasser"));
+                creditsPanel.add(new JLabel("BERDJANE Abderrahmane Mohamed"));
+                creditsPanel.add(new JLabel("BEGGAR Yacine"));
+                creditsPanel.add(new JLabel("ZERAOULIA Ahmed"));
+                creditsFrame.add(creditsPanel);
+
+                creditsFrame.setLocationRelativeTo(null);
+                creditsFrame.setVisible(true);
+            }
+        });
 
         chessBoard = new JPanel(new GridLayout(0, 9)) {
 
-            /**
-             * Override the preferred size to return the largest it can, in
-             * a square shape.  Must (must, must) be added to a GridBagLayout
-             * as the only component (it uses the parent as a guide to size)
-             * with no GridBagConstaint (so it is centered).
-             */
             @Override
             public final Dimension getPreferredSize() {
                 Dimension d = super.getPreferredSize();
@@ -67,10 +89,10 @@ public class ChessGUI {
                 Component c = getParent();
                 if (c == null) {
                     prefSize = new Dimension(
-                            (int)d.getWidth(),(int)d.getHeight());
-                } else if (c!=null &&
-                        c.getWidth()>d.getWidth() &&
-                        c.getHeight()>d.getHeight()) {
+                            (int) d.getWidth(), (int) d.getHeight());
+                } else if (c != null &&
+                        c.getWidth() > d.getWidth() &&
+                        c.getHeight() > d.getHeight()) {
                     prefSize = c.getSize();
                 } else {
                     prefSize = d;
@@ -78,16 +100,15 @@ public class ChessGUI {
                 int w = (int) prefSize.getWidth();
                 int h = (int) prefSize.getHeight();
                 // the smaller of the two sizes
-                int s = (w>h ? h : w);
-                return new Dimension(s,s);
+                int s = (w > h ? h : w);
+                return new Dimension(s, s);
             }
         };
         chessBoard.setBorder(new CompoundBorder(
-                new EmptyBorder(8,8,8,8),
-                new LineBorder(Color.BLACK)
-                ));
+                new EmptyBorder(8, 8, 8, 8),
+                new LineBorder(Color.BLACK)));
         // Set the BG to be ochre
-        Color ochre = new Color(204,119,34);
+        Color ochre = new Color(204, 119, 34);
         chessBoard.setBackground(ochre);
         JPanel boardConstrain = new JPanel(new GridBagLayout());
         boardConstrain.setBackground(ochre);
@@ -106,7 +127,7 @@ public class ChessGUI {
                         new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB));
                 b.setIcon(icon);
                 if ((jj % 2 == 1 && ii % 2 == 1)
-                        //) {
+                        // ) {
                         || (jj % 2 == 0 && ii % 2 == 0)) {
                     b.setBackground(Color.WHITE);
                 } else {
@@ -124,14 +145,14 @@ public class ChessGUI {
         for (int ii = 0; ii < 8; ii++) {
             chessBoard.add(
                     new JLabel(COLS.substring(ii, ii + 1),
-                    SwingConstants.CENTER));
+                            SwingConstants.CENTER));
         }
         // fill the black non-pawn piece row
         for (int ii = 0; ii < 8; ii++) {
             for (int jj = 0; jj < 8; jj++) {
                 switch (jj) {
                     case 0:
-                        chessBoard.add(new JLabel("" + (9-(ii + 1)),
+                        chessBoard.add(new JLabel("" + (9 - (ii + 1)),
                                 SwingConstants.CENTER));
                     default:
                         chessBoard.add(chessBoardSquares[jj][ii]);
@@ -159,7 +180,7 @@ public class ChessGUI {
             System.exit(1);
         }
     }
-    
+
     /**
      * Initializes the icons of the initial chess board piece places
      */
@@ -192,24 +213,18 @@ public class ChessGUI {
             public void run() {
                 ChessGUI cg = new ChessGUI();
 
-                JFrame f = new JFrame("ChessChamp");
+                JFrame f = new JFrame("Chess Game");
                 f.add(cg.getGui());
-                // Ensures JVM closes after frame(s) closed and
-                // all non-daemon threads are finished
                 f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                // See https://stackoverflow.com/a/7143398/418556 for demo.
                 f.setLocationByPlatform(true);
 
-                // ensures the frame is the minimum size it needs to be
-                // in order display the components within it
                 f.pack();
-                // ensures the minimum size is enforced.
+
                 f.setMinimumSize(f.getSize());
                 f.setVisible(true);
             }
         };
-        // Swing GUIs should be created and updated on the EDT
-        // http://docs.oracle.com/javase/tutorial/uiswing/concurrency
         SwingUtilities.invokeLater(r);
     }
+    
 }
